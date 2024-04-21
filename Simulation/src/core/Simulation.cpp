e89bf4be32e980ec36002713becbe122d2d63f03
WittoCMD <vlucic444@gmail.com>
2024-04-21 20:38:57 +0200

    Hello

--- Simulation/src/core/Simulation.cpp
+++ Simulation/src/core/Simulation.cpp
@@ -6,4 +6,5 @@ int main()
     std::cout << "Hello World!\n" << std::endl;
     PrecisionDouble test(1, 5);
     std::cout << test.ToString() << std::endl;
+    std::cout << "Hello" << std::endl;
 }
