ff67ea41b400facec9e87568c238cb1daaffd2ae
SirMrManuel0 <139504269+SirMrManuel0@users.noreply.github.com>
2024-04-24 15:16:48 +0200

    Update: Point3D & Vector3D now use PrecisionDouble

--- Simulation/src/include/geometry/definition/Point3D.cpp
+++ Simulation/src/include/geometry/definition/Point3D.cpp
@@ -1,7 +1,8 @@
 #include "../Point3D.h"
+#include "../../algebra/PrecisionDouble.h"
 
-Point3D::Point3D() : x(0), y(0), z(0) {}
-Point3D::Point3D(double x, double y, double z) : x(x), y(y), z(z) {}
+Point3D::Point3D() : x(PrecisionDouble(0,0)), y(PrecisionDouble(0,0)), z(PrecisionDouble(0,0)) {}
+Point3D::Point3D(PrecisionDouble x, PrecisionDouble y, PrecisionDouble z) : x(x), y(y), z(z) {}
 
 Point3D Point3D::operator+ (Vector3D& vec) const
 {
@@ -30,6 +31,6 @@ Vector3D Point3D::operator- (Point3D& p2) const
 	return Vector3D(x - p2.GetX(), y - p2.GetY(), z - p2.GetZ());
 }
 
-double Point3D::GetX() const { return x; }
-double Point3D::GetY() const { return y; }
-double Point3D::GetZ() const { return z; }
+PrecisionDouble Point3D::GetX() const { return x; }
+PrecisionDouble Point3D::GetY() const { return y; }
+PrecisionDouble Point3D::GetZ() const { return z; }
